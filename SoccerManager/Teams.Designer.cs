94f70e51f2dfba1df3ea91b0aca179e810687156
Wim Everts <wim.everts@gmail.com>
2013-02-21 16:58:00 +0100

    Teams

--- SoccerManager/Teams.Designer.cs
+++ SoccerManager/Teams.Designer.cs
@@ -249,6 +249,7 @@
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(183, 21);
             this.comboBox1.TabIndex = 21;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Teams
             // 
--- SoccerManager/Teams.cs
+++ SoccerManager/Teams.cs
@@ -11,9 +11,26 @@ namespace SoccerManager
 {
     public partial class Teams : Form
     {
+        SoccerWebservice.SoccerManager_WebserviceSoapClient wsSoccer = new SoccerWebservice.SoccerManager_WebserviceSoapClient();
+        DataSet dsTeams;
         public Teams()
         {
             InitializeComponent();
+            voegTeamsToe();
+        }
+
+        private void voegTeamsToe()
+        {
+            dsTeams = wsSoccer.SelectTeamNaam();
+            this.comboBox1.DataSource = dsTeams.Tables[0];
+            this.comboBox1.DisplayMember = "naam";
+            this.comboBox1.ValueMember = "naam";
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
